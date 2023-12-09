8565194b72e144df0d4375505739bf0bfa6039ee
cavidan-1907 <cavidanvelizade9@gmail.com>
2023-12-09 20:01:38 +0400

    add

--- add.js
+++ add.js
@@ -11,36 +11,36 @@ let Addbtn = document.querySelector("#addProduct");
 spinner.style.display = "none";
 
 Addbtn.addEventListener("click", () => {
+    spinner.style.display = "none";
     let nameValueAdd = nameInp.value.trim();
     let unitPraceValueAdd = priceInp.value.trim();
     let unitsInStockValueAdd = stockInp.value.trim();
 
     if (nameValueAdd === "" || unitPraceValueAdd === "" || unitsInStockValueAdd === "") {
-        error.style.display = 'flex';
-        setTimeout(() => {
-           
-            error.style.display = 'none';
-        }, 2000);
-        return;
+   error.style.display="flex";
+   setTimeout(() => {
+    error.style.display="none"
+   },3000);
+   return;
     }
+    error.style.display="none";
+    spinner.style.display="flex";
 
 
-    error.style.display = 'none';
-    spinner.style.display = "block";
-    spinner.style.width="100%"
-
-    axios.post("https://northwind.vercel.app/api/products", {
-            name: nameValueAdd,
-            unitPrice: unitPraceValueAdd,
-            unitsInStock: unitsInStockValueAdd,
-        })
-        .then((res) => {
-            setTimeout(() => {
-                window.location = "./index.html";
-            }, 3000);
-            spinner.style.display = "none";
-            done.innerHTML += `ID nömrəsi : ${res.data.id}`;
-            done.style.display = "flex";
-        });
+    axios.post("https://northwind.vercel.app/api/products",{
+        name: nameValueAdd,
+        unitPrice:unitPraceValueAdd,
+        unitsInStock: unitsInStockValueAdd
+    })
+.then((res)=>{
+setTimeout(() => {
+    window.location="./index.html";
+},3000);
+spinner.style.display="none";
+done.innerHTML+=`ID Nömrəsi : ${res.data.id}`;
+done.style.display="flex"
 });
-console.log("Cavidan");
+
+
+
+});
